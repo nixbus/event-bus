c0cb93dc611d539ecba024e61746a3443d2d496c
David Rey <david.rey.acosta@gmail.com>
2024-06-19 22:21:12 +0200

    test: in memory events mark as finished

--- tests/infrastructure/InMemoryNixEvents.spec.ts
+++ tests/infrastructure/InMemoryNixEvents.spec.ts
@@ -23,6 +23,34 @@ test.describe('InMemoryNixEvents', () => {
     expect(events).verify()
   })
 
+  test('mark the event as finished', async () => {
+    const retryTimeoutInSeconds = 2
+    const event = {
+      id: 'another_event_id',
+      type: 'another_event_type',
+      payload: { key: 'value_2' },
+      created_at: new Date(),
+      updated_at: new Date(),
+    }
+    const subscriber = {
+      id: 'a_subscriber_id',
+      config: { maxRetries: 3, timeout: retryTimeoutInSeconds, concurrency: 1 },
+    }
+
+    await inMemoryNixEvents.subscribe('another_event_type', subscriber)
+    await inMemoryNixEvents.put({ event })
+    await inMemoryNixEvents.findNextEventsFor(subscriber)
+
+    await inMemoryNixEvents.markAsFinished({
+      event,
+      subscriber,
+    })
+
+    const events = await inMemoryNixEvents.getAllEventsTypesAndPayloads()
+
+    expect(events).verify()
+  })
+
   test('mark the event as failed keep the event to retry', async () => {
     const retryTimeoutInSeconds = 2
     const event = {
